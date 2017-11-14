bf34b72fa493113b9b852bc19f1f0c9600d8363c
Frère Jacques <zyb@zjex.com>
2017-11-14 11:20:44 +0800

    fix

---
+++ app/controllers/application.js
@@ -0,0 +1,25 @@
+import Controller from '@ember/controller';
+import  { computed }  from '@ember/object';
+
+export default Controller.extend({
+    actions: {
+        toggleExpandedItem(value, ev) {
+            if (this.get('expandedItem') === value) {
+                value = null;
+            }
+            this.set('expandedItem', value);
+            ev.stopPropagation();
+        }
+    },
+
+    expandedItem: computed('currentRouteName', function() {
+        if (this.get('currentRouteName').substr(0, 6) === 'layout') {
+            return 'layout';
+        } else {
+            return 'demos';
+        }
+    }),
+
+    demosExpanded: computed.equal('expandedItem', 'demos'),
+    layoutExpanded: computed.equal('expandedItem', 'layout')
+});
---
+++ app/templates/route.js
@@ -0,0 +1,9 @@
+
+import Route from '@ember/routing/route';
+import * as computed from '@ember/object/computed';
+
+export default Route.extend({
+    actions: {
+    }
+
+});
